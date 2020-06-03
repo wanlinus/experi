e7d2daf058bfcfaeea20de9507b432e479bb9f6c
wanli1126 <wan.li1@intellif.com>
2020-06-03 20:13:07 +0800

    init commit

---
+++ auth/src/main/java/cn/wanli/auth/controller/HomeController.java
@@ -0,0 +1,20 @@
+package cn.wanli.auth.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public String home() {
+        return "welcome to experi project";
+    }
+
+    @GetMapping("/home")
+    public String asd() {
+        return "this is home api";
+    }
+}
