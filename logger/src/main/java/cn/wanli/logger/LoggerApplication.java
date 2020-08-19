a80c70fa34ca1083c68375606ce999f305bc3f1d
wanli1126 <wan.li1@intellif.com>
2020-08-19 19:52:29 +0800

    feature: 添加日志模块

---
+++ logger/src/main/java/cn/wanli/logger/LoggerApplication.java
@@ -0,0 +1,11 @@
+package cn.wanli.logger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LoggerApplication.class, args);
+    }
+}
---
+++ logger/src/main/java/cn/wanli/logger/mg/MessageConsumer.java
@@ -0,0 +1,11 @@
+package cn.wanli.logger.mg;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageConsumer {
+
+
+
+}
